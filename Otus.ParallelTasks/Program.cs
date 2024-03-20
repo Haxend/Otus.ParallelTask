a76901ed6c9b6ce55e1e04b1acc43b131b937747
Haxend <dunaevskyilya@gmail.com>
2024-03-20 19:41:52 +0300

    Complete Homework

---
+++ Otus.ParallelTasks/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Diagnostics;
+using Otus.ParallelTasks.Services;
+
+var stopwatch = new Stopwatch();
+stopwatch.Start();
+
+IFileResearchService fileResearchService = new FileResearchService();
+var spacesCount = await fileResearchService.CalculateSpacesInFilesAsync("./TextFiles");
+
+stopwatch.Stop();
+
+Console.WriteLine($"Spaces count: {spacesCount}");
+Console.WriteLine($"Elapsed total seconds: {stopwatch.Elapsed.TotalSeconds}");
---
+++ Otus.ParallelTasks/Services/FileResearchService.cs
@@ -0,0 +1,21 @@
+﻿namespace Otus.ParallelTasks.Services;
+
+public class FileResearchService : IFileResearchService
+{
+    public async Task<int> CalculateSpacesInFilesAsync(string directoryPath, CancellationToken token = default)
+    {
+        if (!Directory.Exists(directoryPath))
+            throw new DirectoryNotFoundException($"Directory {directoryPath} not found");
+        
+        var allFilePaths = Directory.GetFiles(directoryPath);
+        var tasks = allFilePaths.Select(CalculateSpacesInFileAsync).ToList();
+        var results = await Task.WhenAll(tasks);
+        return results.Sum();
+    }
+
+    private static async Task<int> CalculateSpacesInFileAsync(string filePath)
+    {
+        var content = await File.ReadAllTextAsync(filePath);
+        return content.Count(e => e == ' ');
+    }
+}
